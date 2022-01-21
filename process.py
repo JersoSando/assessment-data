9275879b11ac1325cff36c64c23b2d768c1df2b0
Jerso Sando <jerso.sando@gmail.com>
2022-01-21 12:06:15 -0700

    final commit

--- process.py
+++ process.py
@@ -2,11 +2,21 @@ log_file = open("um-server-01.txt") #importing the um-server-01.text file and sa
 
 
 def sales_reports(log_file): #creating a function called sales_reports and having it take in a parameter log_file
-    for line in log_file:#looping over each line of a file (loop over list)
-        line = line.rstrip()#gets rid of all the whitespace
-        day = line[0:3]#checks the first 3 letters of the list and returns a string
-        if day == "Mon":#if the day (the first three letters) is Tue
-            print(line)#print the line 'Tue' as a string
+    for line in log_file: #looping over each line of a file (loop over list)
+        line = line.rstrip() #gets rid of all the whitespace
+        day = line[0:3] #checks the first 3 letters of the list and returns a string
+        if day == "Mon": #if the day (the first three letters) is Tue
+            print(line) #print the line 'Tue' as a string
 
 
 sales_reports(log_file)#invokes the sales_reports function
+
+
+def melon_orders(log_file):
+    for line in log_file:
+        line = line.rstrip().split('')
+        count = int(line[2])
+        if count > 10:
+            print(line)
+
+melon_orders(log_file)
--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,6 +1,46 @@
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
 
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = {
+    getCountries: (req, res) => {
+        sequelize.query(`SELECT * FROM countries;`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+
+    },
+    createCity: (req, res) => {
+        const {name, rating, countryId} = req.body
+        sequelize.query(`INSERT INTO cities (name, rating, country_id) VALUES ('${name}', '${rating}', '${countryId}')`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+    getCities: (req, res) => {
+        sequelize.query(`SELECT ci.city_id, ci.name AS city, ci.rating, co.country_id, co.name AS country
+        FROM cities AS ci
+        JOIN countries AS co
+        ON co.country_id = ci.country_id
+        ORDER BY ci.rating DESC;`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+    deleteCity: (req, res) => {
+        const {id} = req.params
+        sequelize.query(`DELETE FROM cities 
+        WHERE city_id = ${id}`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.timeLog(err))
+    },
     seed: (req, res) => {
         sequelize.query(`
             drop table if exists cities;
@@ -11,7 +51,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            CREATE TABLE cities (
+                city_id SERIAL PRIMARY KEY,
+                name VARCHAR(100),
+                rating INTEGER,
+                country_id INTEGER NOT NULL REFERENCES countries(country_id)
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
@@ -209,6 +254,9 @@ module.exports = {
             ('Yemen'),
             ('Zambia'),
             ('Zimbabwe');
+
+            // insert into cities (name, rating, country_id)
+            // values ('Kampala', 3,  )
         `).then(() => {
             console.log('DB seeded!')
             res.sendStatus(200)
