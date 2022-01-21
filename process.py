5938bb52d539ff803dbdd00b9bfa2605ad3c3532
Jerso Sando <jerso.sando@gmail.com>
2022-01-21 09:24:01 -0700

    initialized git

---
+++ process.py
@@ -0,0 +1,12 @@
+log_file = open("um-server-01.txt") #importing the um-server-01.text file and saving it to a variable
+
+
+def sales_reports(log_file): #creating a function called sales_reports and having it take in a parameter log_file
+    for line in log_file:#looping over each line of a file (loop over list)
+        line = line.rstrip()#gets rid of all the whitespace
+        day = line[0:3]#checks the first 3 letters of the list and returns a string
+        if day == "Mon":#if the day (the first three letters) is Tue
+            print(line)#print the line 'Tue' as a string
+
+
+sales_reports(log_file)#invokes the sales_reports function
